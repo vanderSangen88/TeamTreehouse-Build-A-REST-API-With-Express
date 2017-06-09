026893d3df8b0a6ba099ff96ffc6cfb1846420fb
NickvanderSangen <nick.vandersangen@theapsgroup.com>
2017-06-09 16:45:59 +0200

    End of 2.6 Error Handling in Express

--- qa-rest-api/app.js
+++ qa-rest-api/app.js
@@ -13,6 +13,24 @@ app.use(bodyParser.xml());
 
 app.use("/questions", routes);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next){
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next){
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: err.message
+		}
+	});
+});
+
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
--- qa-rest-api/routes.js
+++ qa-rest-api/routes.js
@@ -60,7 +60,17 @@ router.delete("/:qID/answers/:aID", function(req, res){
 // POST /questions/:id/answers/:id/vote-up
 // POST /questions/:id/answers/:id/vote-down
 // Vote on a specific answer
-router.post("/:qID/answers/:aID/vote-:dir", function(req, res){
+router.post("/:qID/answers/:aID/vote-:dir", 
+	function(req, res, next){
+		if(req.params.dir.search(/^(up|down)$/) === -1) {
+			var err = new Error("Not Found");
+			err.status = 404;
+			next(err);
+		} else {
+			next();
+		}
+	}, 
+	function(req, res){
 	res.json({
 		response: "You sent me a POST request to /vote-" + req.params.dir,
 		questionID: req.params.qID,
