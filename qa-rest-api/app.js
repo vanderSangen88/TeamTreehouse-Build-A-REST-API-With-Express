6922ebc111bcfd03917c2603a4e57e59c3a27844
NickvanderSangen <nick.vandersangen@theapsgroup.com>
2017-06-09 14:58:20 +0200

    End of 2.3 Accepting Data: JSON > XML

--- qa-rest-api/app.js
+++ qa-rest-api/app.js
@@ -2,11 +2,21 @@
 
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+require('body-parser-xml')(bodyParser);
 
-
-app.use(function(req, res, next){
+var xmlCheck = function(req, res, next){
+	if(req.body){
+		console.log("The sky is", req.body.color);
+	} else {
+		console.log("There is no body property on the request");
+	}
 	next();
-});
+}
+
+app.use(xmlCheck);
+app.use(bodyParser.xml());
+app.use(xmlCheck);
 
 var port = process.env.PORT || 3000;
 
