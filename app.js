added382c87febae5f6a6ee37c91f0a14c622452
NickvanderSangen <nick.vandersangen@theapsgroup.com>
2017-06-09 13:51:49 +0200

    First setup of middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(function(req, res, next){
+	console.log("First piece of middleware");
+	next();
+});
+app.use(function(req, res, next){
+	console.log("Second piece of middleware");
+	next();
+});
+
 app.listen(port, function(){
 	console.log("Express server is listening on port", port);
 }); 
+
