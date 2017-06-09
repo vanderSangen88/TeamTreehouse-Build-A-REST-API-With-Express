811e6ee172c36e71edcb29b7ddb76d41e7e7211d
NickvanderSangen <nick.vandersangen@theapsgroup.com>
2017-06-09 14:14:06 +0200

    Fixed resource

--- qa-rest-api/app.js
+++ qa-rest-api/app.js
@@ -2,8 +2,18 @@
 
 var express = require("express");
 var app = express();
+
+app.use(function(req, res, next){
+	console.log("First piece of middleware");
+	next();
+});
+app.use("different", function(req, res, next){
+	console.log("Second piece of middleware");
+	next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
 	console.log("Express server is listening on port", port);
-});
+});  
